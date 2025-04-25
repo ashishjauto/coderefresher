4617d06a805e6be2c9abce8bf35d068a9ee7d9be
ashishjauto <ashishjaiswalcs@gmail.com>
2025-04-25 12:26:47 +0530

    initial commit

---
+++ src/main/java/com/code/auto/firstInteger/Palindrome.java
@@ -0,0 +1,45 @@
+package com.code.auto.firstInteger;
+
+public class Palindrome {
+
+
+    /**
+    *
+    *
+    * Find if the number is palindrome or not
+    *
+
+     */
+    public static void main(String[] args) {
+
+        int checkPalindrome=12321;
+
+
+        System.out.println("YOUR RESULT "+checkIntegerPalindrome(checkPalindrome));
+
+
+    }
+    public static boolean checkIntegerPalindrome(int variable){
+
+        if(variable<0 || (variable%10==0 && variable!=0))
+            return false;
+        int reverseNumber=0;
+
+        while (variable> reverseNumber){
+            int pop= variable%10;
+            reverseNumber= reverseNumber*10+pop;
+            variable= variable/10;
+
+        }
+
+
+
+
+        return  variable==reverseNumber||variable== reverseNumber/10;
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/code/auto/secondstring/ReverseString.java
@@ -0,0 +1,34 @@
+package com.code.auto.secondstring;
+
+import java.util.Arrays;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        char[] elemetTobereversed = {'A', 'S', 'H', 'I', 'S', 'H'};
+
+        String value = "Ashish";
+
+        String reversed = Arrays.toString(reverseString(value.toCharArray()));
+
+        System.out.println(reversed);
+
+
+    }
+
+    private static char[] reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+        return  s;
+
+
+    }
+}
